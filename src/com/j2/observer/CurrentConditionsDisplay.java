67c510d0a0016680d1d4fbf00131958e51d17d60
bckang92 <bckang92@naver.com>
2015-10-02 17:17:15 +0900

    observerHomework

--- src/com/j2/observer/CurrentConditionsDisplay.java
+++ src/com/j2/observer/CurrentConditionsDisplay.java
@@ -1,19 +1,21 @@
-package com.j2.observer;
-
-public class CurrentConditionsDisplay implements Observer {
-  private float remperature;
-  private float humidity;
-  private Subject weatherData;
-  public CurrentConditionsDisplay(Subject weatherData) {
-    this.weatherData=weatherData;
-    weatherData.registerObserver(this);
-  }
-  public void update(float temperature, float humidity, float pressure) {
-    this.temperature = temperature;
-    this.humidity = humidity;
-    display();
-  }
-  public void display() {
-    System.out.println("Current conditions: " + temperature + "F degress and " + humidity + "% humidity");
-  }
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	private float temperature;
+	private float humidity;
+	private Subject weatherdata;
+	
+	public CurrentConditionsDisplay(Subject wd) {
+		this.weatherdata = wd;
+		wd.registerObserver(this);
+	}
+	
+	public void update(float temp, float hum, float pres) {
+		this.temperature = temp;
+		this.humidity = hum;
+		display();
+	}
+	
+	public void display() {
+		System.out.println("Current conditions: " + temperature 
+			+ "F degrees and " + humidity + "% humidity");
+	}
 }
--- src/com/j2/observer/WeatherStation.java
+++ src/com/j2/observer/WeatherStation.java
@@ -1,9 +1,19 @@
-package com.j2.observer;
-
+import java.util.*;
 public class WeatherStation {
-  public static void main(String[] args) {
-    WeatherData weatherData=new WeatherData();
-    CurrentConditionsDisplay currentDisplay=new CurrentConditionDisplay(weatherData);
-    weatherData.setMeasurements(80,65,30,4f);
-  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WeatherData weatherData = new WeatherData();
+		
+		CurrentConditionsDisplay currentDisplay = 
+			new CurrentConditionsDisplay(weatherData);
+		//StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		//ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+
+	}
+
 }
