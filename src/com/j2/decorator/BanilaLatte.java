e1cf0245576fe7087ce2e048aad4adcd66a93669
bckang92 <bckang92@naver.com>
2015-10-07 14:45:27 +0900

    decoratorHomework1 final

---
+++ src/com/j2/decorator/BanilaLatte.java
@@ -0,0 +1,11 @@
+package com.j2.decorator;
+public class BanilaLatte extends Beverage {
+  
+ public BanilaLatte() {
+  description = "BanilaLatte";
+ }
+  
+ public double cost() {
+  return 2.10;
+ }
+}
