6fbad9bb35d0700ded58564ccde5da8b40a88008
bckang92 <bckang92@naver.com>
2015-12-05 16:35:21 +0900

    statepattern

---
+++ src/com/j2/state/videoplayer/DisplayOffState.java
@@ -0,0 +1,18 @@
+
+package com.j2.state.videoplayer;
+
+public class DisplayOffState implements State {
+   VideoPlayer videoPlayer;
+ 
+    public DisplayOffState(VideoPlayer videoPlayer) {
+        this.videoPlayer = videoPlayer;
+    }
+ 
+public void playStop(){  System.out.println("play is stopped");}
+ public void displaySleepOn(){System.out.println("displaySleepOn is not executed");}
+ public void displaySleepOff(){System.out.println("displaySleepOff is not executed");}
+ 
+ public void display(){System.out.println("when display is black ....");}
+
+ 
+}
---
+++ src/com/j2/state/videoplayer/State.java
@@ -0,0 +1,11 @@
+
+package com.j2.state.videoplayer;
+
+
+public interface State {
+ public void playStop();
+ public void displaySleepOn();
+ public void displaySleepOff();
+ public void display();
+ 
+}
